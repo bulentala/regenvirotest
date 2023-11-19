3176c43c072b1f4743e4c9085244d826b51b7976
Bülent ALA <46373855+bulentala@users.noreply.github.com>
2023-11-19 11:14:43 +0300

    ok

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,6 +3,7 @@ import { Locale } from "../../i18n-config";
 import LocaleSwitcher from "./components/locale-switcher";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
+import Products from "./components/Products";
 export default async function IndexPage({
   params: { lang },
 }: {
@@ -14,6 +15,7 @@ export default async function IndexPage({
     <div>
       <Hero lang={lang} />
       <AboutUs lang={lang} />
+      <Products lang={lang} />
       <pre>{JSON.stringify(lang, null, 2)}</pre>
       <pre>{JSON.stringify(dictionary, null, 2)}</pre>
       <LocaleSwitcher />
